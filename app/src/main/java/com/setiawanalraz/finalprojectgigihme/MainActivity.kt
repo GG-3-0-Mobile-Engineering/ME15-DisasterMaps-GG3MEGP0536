9d0e458b5435f9c9be68499312cc7be9e60f5a36
Alraz Setiawan <alrazsetiawan79@gmail.com>
2023-07-23 12:45:37 +0700

    Initial commit, add GMaps, add search bar

---
+++ app/src/main/java/com/setiawanalraz/finalprojectgigihme/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.setiawanalraz.finalprojectgigihme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mGoogleMap: GoogleMap? = null
+    private lateinit var autocompleteFragment: AutocompleteSupportFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Places.initialize(applicationContext, getString(R.string.google_map_api_key))
+        autocompleteFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                as AutocompleteSupportFragment
+        autocompleteFragment.setPlaceFields(
+            listOf(
+                Place.Field.ID,
+                Place.Field.ADDRESS,
+                Place.Field.LAT_LNG
+            )
+        )
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onError(p0: Status) {
+                Toast.makeText(this@MainActivity, p0.statusMessage, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPlaceSelected(place: Place) {
+                val latLng = place.latLng
+                zoomOnMap(latLng)
+            }
+
+        })
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment)
+                as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+    }
+
+    private fun zoomOnMap(latLng: LatLng) {
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(latLng, 12f)
+        mGoogleMap?.animateCamera(newLatLngZoom)
+    }
+}
